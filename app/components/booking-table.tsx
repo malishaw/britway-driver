26b8986bd8badce70c6a7fb8d73ed591b1408440
Sanka <sanka.s@fidenz.com>
2024-08-07 13:37:03 +0530

    add start end date filters

--- app/components/booking-table.tsx
+++ app/components/booking-table.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import {
   ColumnDef,
   ColumnFiltersState,
+  FilterFn,
   SortingState,
   VisibilityState,
   flexRender,
@@ -33,72 +34,100 @@ import {
 import { cn } from "@/lib/utils";
 import { Booking } from "../typings";
 
-export const columns: ColumnDef<Booking>[] = [
-  {
-    accessorKey: "journeyDate",
-    header: "Journey Date",
-    filterFn: "includesString",
-  },
-  { accessorKey: "refId", header: "Reference ID" },
-  { accessorKey: "driver", header: "Driver" },
-  { accessorKey: "status", header: "Status" },
-  { accessorKey: "driverIncome", header: "Driver Income" },
-  { accessorKey: "total", header: "Total" },
-  { accessorKey: "payments", header: "Payments" },
-  { accessorKey: "vehicle", header: "Vehicle" },
-  { accessorKey: "vehicleType", header: "Vehicle Type" },
-  { accessorKey: "discount", header: "Discount" },
-  { accessorKey: "passengerName", header: "Passenger Name" },
-  { accessorKey: "flightNumber", header: "Flight Number" },
-  { accessorKey: "flightLandingTime", header: "Flight Landing Time" },
-  { accessorKey: "arrivingFrom", header: "Arriving From" },
-  { accessorKey: "flightDepartureNumber", header: "Flight Departure Number" },
-  { accessorKey: "serviceDuration", header: "Service Duration" },
-  { accessorKey: "serviceType", header: "Service Type" },
-  { accessorKey: "flightDepartureTime", header: "Flight Departure Time" },
-  { accessorKey: "flightDepartureTo", header: "Flight Departure To" },
-  { accessorKey: "phoneNumber", header: "Phone Number" },
-  { accessorKey: "pickup", header: "Pickup" },
-  { accessorKey: "dropoff", header: "Dropoff" },
-  { accessorKey: "via", header: "Via" },
-  { accessorKey: "passengers", header: "Passengers" },
-  { accessorKey: "suitcases", header: "Suitcases" },
-  { accessorKey: "carryOn", header: "Carry On" },
-  { accessorKey: "childSeats", header: "Child Seats" },
-  { accessorKey: "boosterSeats", header: "Booster Seats" },
-  { accessorKey: "infantSeats", header: "Infant Seats" },
-  { accessorKey: "wheelchairs", header: "Wheelchairs" },
-  { accessorKey: "waitingTime", header: "Waiting Time" },
-  { accessorKey: "email", header: "Email" },
-  { accessorKey: "meetGreet", header: "Meet & Greet" },
-  { accessorKey: "source", header: "Source" },
-  { accessorKey: "customer", header: "Customer" },
-  { accessorKey: "departments", header: "Departments" },
-  { accessorKey: "leadName", header: "Lead Name" },
-  { accessorKey: "leadEmail", header: "Lead Email" },
-  { accessorKey: "leadPhoneNumber", header: "Lead Phone Number" },
-  { accessorKey: "createdAt", header: "Created At" },
-  { accessorKey: "updatedAt", header: "Updated At" },
-  { accessorKey: "id", header: "ID" },
-  { accessorKey: "currency", header: "Currency" },
-  {
-    accessorKey: "trackingHistory",
-    header: "Tracking History",
-    cell: ({ getValue }) => (
-      <span className="whitespace-nowrap">{`${getValue()}`}</span>
-    ),
-  },
-];
+declare module "@tanstack/table-core" {
+  interface FilterFns {
+    dateBetweenFilterFn: FilterFn<unknown>;
+  }
+}
+
+const dateBetweenFilterFn: FilterFn<any> = (row, columnId, value) => {
+  const dateTime = row.getValue(columnId) as string;
+  const [start, end] = value;
+  if (!dateTime) {
+    return false;
+  }
+
+  const date = dateTime.split(" ")[0];
+
+  if (start && !end) {
+    return date >= start;
+  }
+  if (!start && end) {
+    return date <= end;
+  }
+  if (start && end) {
+    return date >= start && date <= end;
+  }
+  return true;
+};
 
 export function BookingTable({ data = [] }: { data: Booking[] }) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
   );
-  const [date, setDate] = React.useState<Date>();
+  const [dateFrom, setDateFrom] = React.useState<Date>();
+  const [dateTo, setDateTo] = React.useState<Date>();
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = React.useState({});
+
+  const columns: ColumnDef<Booking>[] = [
+    {
+      accessorKey: "journeyDate",
+      header: "Journey Date",
+      filterFn: "dateBetweenFilterFn",
+    },
+    { accessorKey: "refId", header: "Reference ID" },
+    { accessorKey: "driver", header: "Driver" },
+    { accessorKey: "status", header: "Status" },
+    { accessorKey: "driverIncome", header: "Driver Income" },
+    { accessorKey: "total", header: "Total" },
+    { accessorKey: "payments", header: "Payments" },
+    { accessorKey: "vehicle", header: "Vehicle" },
+    { accessorKey: "vehicleType", header: "Vehicle Type" },
+    { accessorKey: "discount", header: "Discount" },
+    { accessorKey: "passengerName", header: "Passenger Name" },
+    { accessorKey: "flightNumber", header: "Flight Number" },
+    { accessorKey: "flightLandingTime", header: "Flight Landing Time" },
+    { accessorKey: "arrivingFrom", header: "Arriving From" },
+    { accessorKey: "flightDepartureNumber", header: "Flight Departure Number" },
+    { accessorKey: "serviceDuration", header: "Service Duration" },
+    { accessorKey: "serviceType", header: "Service Type" },
+    { accessorKey: "flightDepartureTime", header: "Flight Departure Time" },
+    { accessorKey: "flightDepartureTo", header: "Flight Departure To" },
+    { accessorKey: "phoneNumber", header: "Phone Number" },
+    { accessorKey: "pickup", header: "Pickup" },
+    { accessorKey: "dropoff", header: "Dropoff" },
+    { accessorKey: "via", header: "Via" },
+    { accessorKey: "passengers", header: "Passengers" },
+    { accessorKey: "suitcases", header: "Suitcases" },
+    { accessorKey: "carryOn", header: "Carry On" },
+    { accessorKey: "childSeats", header: "Child Seats" },
+    { accessorKey: "boosterSeats", header: "Booster Seats" },
+    { accessorKey: "infantSeats", header: "Infant Seats" },
+    { accessorKey: "wheelchairs", header: "Wheelchairs" },
+    { accessorKey: "waitingTime", header: "Waiting Time" },
+    { accessorKey: "email", header: "Email" },
+    { accessorKey: "meetGreet", header: "Meet & Greet" },
+    { accessorKey: "source", header: "Source" },
+    { accessorKey: "customer", header: "Customer" },
+    { accessorKey: "departments", header: "Departments" },
+    { accessorKey: "leadName", header: "Lead Name" },
+    { accessorKey: "leadEmail", header: "Lead Email" },
+    { accessorKey: "leadPhoneNumber", header: "Lead Phone Number" },
+    { accessorKey: "createdAt", header: "Created At" },
+    { accessorKey: "updatedAt", header: "Updated At" },
+    { accessorKey: "id", header: "ID" },
+    { accessorKey: "currency", header: "Currency" },
+    {
+      accessorKey: "trackingHistory",
+      header: "Tracking History",
+      cell: ({ getValue }) => (
+        <span className="whitespace-nowrap">{`${getValue()}`}</span>
+      ),
+    },
+  ];
 
   const table = useReactTable({
     data,
@@ -117,47 +146,85 @@ export function BookingTable({ data = [] }: { data: Booking[] }) {
       columnVisibility,
       rowSelection,
     },
+    filterFns: {
+      dateBetweenFilterFn: dateBetweenFilterFn,
+    },
   });
 
   React.useEffect(() => {
-    if (date) {
-      table
-        .getColumn("journeyDate")
-        ?.setFilterValue(format(date, "dd/MM/yyyy"));
-    } else {
-      table.getColumn("journeyDate")?.setFilterValue("");
-    }
-  }, [date]);
+    table
+      .getColumn("journeyDate")
+      ?.setFilterValue([
+        dateFrom ? format(dateFrom, "dd/MM/yyyy") : "",
+        dateTo ? format(dateTo, "dd/MM/yyyy") : "",
+      ]);
+  }, [dateFrom, dateTo]);
 
   return (
     <div className="overflow-auto">
-      <div className="flex items-center pb-4 ">
+      <div className="flex items-center gap-5 pb-4 ">
         <Popover>
           <PopoverTrigger asChild>
             <Button
               variant={"outline"}
               className={cn(
                 "w-[280px] justify-start text-left font-normal",
-                !date && "text-muted-foreground"
+                !dateFrom && "text-muted-foreground"
               )}
             >
               <CalendarIcon className="mr-2 h-4 w-4" />
-              {date ? format(date, "PPP") : <span>Pick a date</span>}
+              {dateFrom ? (
+                format(dateFrom, "PPP")
+              ) : (
+                <span>Pick a Start Date</span>
+              )}
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-auto p-0">
             <Calendar
               mode="single"
-              selected={date}
-              onSelect={setDate}
+              selected={dateFrom}
+              onSelect={setDateFrom}
               initialFocus
             />
           </PopoverContent>
         </Popover>
 
-        <Button className="ml-5" onClick={() => setDate(undefined)}>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-[280px] justify-start text-left font-normal",
+                !dateTo && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {dateTo ? format(dateTo, "PPP") : <span>Pick a End Date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode="single"
+              selected={dateTo}
+              onSelect={setDateTo}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+
+        <Button
+          onClick={() => {
+            setDateFrom(undefined);
+            setDateTo(undefined);
+          }}
+        >
           Clear Filter
         </Button>
+
+        <p className="ml-auto">
+          {table.getFilteredRowModel().rows.length} Bookings
+        </p>
       </div>
       <div className="rounded-md border">
         <Table>
